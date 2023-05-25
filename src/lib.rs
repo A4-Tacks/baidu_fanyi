20a8e098a98ffc3aa80f75f267bede63acaf513a
A4-Tacks <wdsjxhno1001@163.com>
2023-05-25 22:19:35 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,179 @@
+pub mod mini_fmt {
+    use std::fmt::{Display, Debug};
+
+    #[derive(Debug, Clone, Copy)]
+    pub enum FmtStyle {
+        /// ToString (Display)
+        Str,
+        /// Repr (Debug)
+        Repr,
+        /// Expand repr (Debug)
+        ERepr,
+    }
+    impl FmtStyle {
+        pub fn fmt_str<S>(self, str: S) -> String
+            where S: Debug + Display
+        {
+            match self {
+                Self::Str => format!("{}", str),
+                Self::Repr => format!("{:?}", str),
+                Self::ERepr => format!("{:#?}", str),
+            }
+        }
+    }
+    #[derive(Debug, Clone)]
+    pub enum FmtType {
+        Const(String),
+        Value { style: FmtStyle },
+        IndexValue { id: usize, style: FmtStyle },
+    }
+    impl Default for FmtType {
+        fn default() -> Self {
+            Self::Const(String::default())
+        }
+    }
+    impl FmtType {
+        /// 格式化并移动格式化指针
+        fn fmt_str<S>(&self, idx: &mut usize, args: &[S]) -> String
+            where S: Display + Debug
+        {
+            use FmtType::*;
+            let res = format!("{}", match self {
+                Const(s) => s.into(),
+                Value { style } => {
+                    let tmp_idx = *idx;
+                    *idx += 1;
+                    style.fmt_str(&args[tmp_idx])
+                },
+                FmtType::IndexValue { id, style } => style.fmt_str(&args[*id]),
+            });
+            res
+        }
+    }
+    /// 动态的格式化输入
+    /// # Examples
+    /// ```
+    /// use baidu_fanyi::mini_fmt::Fmtter;
+    /// let fmtter = Fmtter::build("ab%sde").unwrap();
+    /// assert_eq!(&fmtter.fmt_str(&["c"]), "abcde");
+    /// assert_eq!(&fmtter.fmt_str(&[7]), "ab7de");
+    ///
+    /// let fmtter = Fmtter::build("%s,%s,%0s,%1r,%s").unwrap();
+    /// dbg!(&fmtter);
+    /// assert_eq!(&fmtter.fmt_str(&["a", "b", "c"]), "a,b,a,\"b\",c");
+    /// ```
+    /// |----|-------------|
+    /// | %s | Display     |
+    /// | %r | Debug       |
+    /// | %R | DebugExpand |
+    /// | %n | LF          |
+    /// | %N | CR          |
+    /// | %t | Tab         |
+    /// | %e | ESC         |
+    /// |----|-------------|
+    ///
+    /// `%[n]...` example: `%0s`, index 0 Display
+    #[derive(Debug, Default)]
+    pub struct Fmtter {
+        args: Vec<FmtType>,
+    }
+    impl From<Vec<FmtType>> for Fmtter {
+        fn from(args: Vec<FmtType>) -> Self {
+            Self { args }
+        }
+    }
+    impl TryFrom<&str> for Fmtter {
+        type Error = String;
+        fn try_from(value: &str) -> Result<Self, Self::Error> {
+            Self::build(value)
+        }
+    }
+    impl Fmtter {
+        /// new empty
+        pub fn new() -> Self {
+            Self::default()
+        }
+        /// from str build
+        pub fn build(fmtter: &str) -> Result<Self, String> {
+            let mut chars = fmtter.chars();
+            let mut args: Vec<FmtType> = Vec::new();
+            let mut last_val = String::new();
+            while let Some(c) = chars.next() {
+                macro_rules! add {
+                    ( $val:expr ) => {{
+                        if last_val.len() != 0 {
+                            // 仅当前方有一个不为空的常量串时进行添加
+                            args.push(FmtType::Const(last_val));
+                            last_val = String::new();
+                        }
+                        args.push($val);
+                    }};
+                }
+                macro_rules! res_seq_in_end {
+                    () => {{
+                        return Err("sequence in fmtter end".into());
+                    }};
+                }
+                macro_rules! get_seq {
+                    () => {{
+                        if let Some(x) = chars.next() {
+                            x
+                        } else {
+                            res_seq_in_end!()
+                        }
+                    }};
+                }
+                /// 完成最终转义序列的匹配
+                macro_rules! style_pat {
+                    ( $val:expr ) => {{
+                        match $val {
+                            's' => FmtStyle::Str,
+                            'r' => FmtStyle::Repr,
+                            'R' => FmtStyle::ERepr,
+                            x => return Err(
+                                format!("unknown sequence: {:?}", x)),
+                        }
+                    }};
+                }
+                match c {
+                    '%' => {
+                        let next_c = get_seq!();
+                        match next_c {
+                            // 中间匹配或者截断
+                            x @ '0'..='9' => {
+                                // 元素位置引用 (没有支持10及以上的打算)
+                                add!(FmtType::IndexValue {
+                                    id: x.to_digit(10).unwrap() as usize,
+                                    style: style_pat!(get_seq!())
+                                })
+                            },
+                            '%' => last_val.push(c), // 普通的百分号
+                            'n' => last_val.push('\n'), // 换行
+                            'N' => last_val.push('\r'), // 回车
+                            't' => last_val.push('\t'), // 制表
+                            'e' => last_val.push('\x1b'), // ESC
+                            _ => add!(FmtType::Value {
+                                style: style_pat!(next_c)
+                            }),
+                        }
+                    },
+                    _ => {
+                        last_val.push(c);
+                    },
+                }
+            }
+            if last_val.len() != 0 {
+                args.push(FmtType::Const(last_val));
+            }
+            Ok(args.into())
+        }
+        pub fn fmt_str<S: Display + Debug>(&self, strs: &[S]) -> String {
+            let mut res = String::new();
+            let mut idx = 0;
+            for i in &self.args {
+                res.push_str(&i.fmt_str(&mut idx, strs));
+            }
+            res
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,411 @@
+use std::{
+    collections::HashMap,
+    env::{
+        self, 
+        args
+    },
+    ffi::OsString,
+    fs::{
+        self, 
+        read_to_string
+    },
+    io::{
+        stdin, 
+        Read
+    },
+    path::PathBuf,
+    process::exit,
+};
+
+use baidu_fanyi::mini_fmt::Fmtter;
+use reqwest::{
+    header::HeaderMap,
+    Client,
+    Error as RError,
+    Response,
+};
+use serde_json::Value;
+use lazy_static::lazy_static;
+use md5::{
+    self,
+    Digest
+};
+use rand::random;
+
+
+macro_rules! literals {
+    ( $(
+            #define $name:ident = $value:expr;
+    )* ) => {
+        $(
+            macro_rules! $name {
+                () => {
+                    $value
+                }
+            }
+        )*
+    };
+}
+literals!{
+    #define API_END_POINT = "http://api.fanyi.baidu.com";
+    #define API_INTERFACE_PATH = "/api/trans/vip/translate";
+}
+
+
+pub const URL: &str = concat!(API_END_POINT!(), API_INTERFACE_PATH!());
+pub const DEFAULT_FROM_LANG: &str = "auto";
+pub const DEFAULT_TO_LANG: &str = "auto";
+pub const MAX_TIMEOUT_COUNT: u32 = 2;
+pub const MAX_ERROR_COUNT: u32 = 2;
+pub const MAX_REQUEST_BYTES: usize = 4000;
+
+
+lazy_static!{
+    static ref HEADERS: HeaderMap = {
+        let mut x = HeaderMap::new();
+        // 'Content-Type': 'application/x-www-form-urlencoded'
+        x.insert("Content-Type",
+                 "application/x-www-form-urlencoded".parse().unwrap());
+        x
+    };
+}
+
+
+/// 传入累计大小
+/// 修改大小计数并且返回是否需要分配新的一个块
+pub fn split_blocks(sum: &mut usize, this: usize) -> Result<bool, ()> {
+    if this < MAX_REQUEST_BYTES {
+        let num = *sum + this;
+        Ok(if num < MAX_REQUEST_BYTES {
+            false
+        } else {
+            // 旧子块加上新子块超出了最大块大小
+            // 将统计大小赋值为新块大小并通知新建块
+            // 比较的子块将被放入新块
+            *sum = this;
+            true
+        })
+    } else {
+        // 大于最大请求大小
+        Err(())
+    }
+}
+
+
+type JSONData = HashMap<String, Value>;
+
+
+/// 构建 md5 值, 官方示例是 utf-8 编码, 而 rust 字符串为 utf-8, 因此不用转换
+fn make_md5(s: &str) -> Digest {
+    md5::compute(s.as_bytes())
+}
+
+
+/// 获取盐值
+/// 官方要求盐值在 [32768,65536], 实在是阴间
+/// 我推测可能是 [32768,65536) 因此使用我推测的值
+fn get_salt() -> u16 {
+    const MASK: u16 = 32767;
+    const BASE: u16 = MASK + 1;
+    let rand_num: u16 = random();
+    let final_num = (rand_num & MASK) + BASE;
+    final_num
+}
+
+
+async fn post(
+    url: &str,
+    headers: HeaderMap,
+    data: &JSONData
+    ) -> Result<Response, RError> {
+    let client = Client::new();
+    client.post(url)
+        .headers(headers)
+        .form(data)
+        .send().await
+}
+
+
+fn get_id_and_key() -> [String; 2] {
+    let path = config_path();
+    let file: String = fs::read_to_string(&path)
+        .unwrap_or_else(|e| {
+            eprintln!("read config file error. path: {:?}, msg: {:?}",
+                      &path, e.to_string());
+            panic!();
+        });
+    let mut lines = file.lines();
+    let msg: &str = "config lines < 2";
+    [lines.next().expect(msg).into(), lines.next().expect(msg).into()]
+}
+
+fn config_path() -> OsString {
+    let mut path = PathBuf::new();
+    path.push(&env::var("HOME").expect("get home error")[..]);
+    path.push(".baidufanyi_key");
+    path.into()
+}
+
+
+#[derive(Clone, Copy)]
+struct Translater<'a> {
+    id: &'a str,
+    key: &'a str,
+    salt: u16,
+    from_lang: &'a str,
+    to_lang: &'a str,
+}
+impl<'a> Translater<'a> {
+    pub fn new(id: &'a str, key: &'a str) -> Self {
+        Self {
+            id,
+            key,
+            salt: 0,
+            from_lang: DEFAULT_FROM_LANG,
+            to_lang: DEFAULT_TO_LANG,
+        }
+    }
+
+    pub fn set_from_lang(&mut self, from: &'a str) -> &Self {
+        self.from_lang = from;
+        self
+    }
+
+    pub fn set_to_lang(&mut self, to: &'a str) -> &Self {
+        self.to_lang = to;
+        self
+    }
+
+    /// 更新盐值
+    pub fn update_salt(&mut self) {
+        self.salt = get_salt()
+    }
+
+    /// 构建请求荷载
+    /// 'appid': appid,
+    /// 'q': query,
+    /// 'from': from_lang,
+    /// 'to': to_lang,
+    /// 'salt': salt,
+    /// 'sign': sign
+    pub fn build_payload(&self, message: String) -> JSONData {
+        const KEY_COUNT: usize = 6;
+        let sign = self.get_sign(&message); // 初始化签名
+        let mut data = JSONData::with_capacity(KEY_COUNT);
+        debug_assert!(data.capacity() >= KEY_COUNT); // 可能分配更多
+
+        data.insert("appid".to_string(), self.id.into());
+        data.insert("q".into(), message.into());
+        data.insert("from".into(), self.from_lang.into());
+        data.insert("to".into(), self.to_lang.into());
+        data.insert("salt".into(), self.salt.into());
+        data.insert("sign".into(), sign.into());
+        data
+    }
+
+    /// 请求翻译
+    pub async fn translate(&mut self, message: String) -> JSONData {
+        self.update_salt(); // 需要先初始化盐值
+        let payload: JSONData = self.build_payload(message);
+        let mut timeout_count: u32 = 0;
+        let mut error_count: u32 = 0;
+        let result = loop {
+            match post(URL, HEADERS.clone(), &payload).await {
+                Ok(val) => break val,
+                Err(e) => {
+                    if e.is_timeout() {
+                        timeout_count += 1
+                    } else {
+                        error_count += 1
+                    }
+                    if timeout_count >= MAX_TIMEOUT_COUNT {
+                        panic!("timeout count >= {}", MAX_TIMEOUT_COUNT)
+                    }
+                    if error_count >= MAX_ERROR_COUNT {
+                        panic!("error count >= {}", MAX_ERROR_COUNT)
+                    }
+                }
+            }
+        };
+        result.json::<JSONData>().await.expect("data to json error")
+    }
+
+    /// 构建 md5 签名, 官方示例组合方式为
+    /// appid + query + salt + appkey
+    /// salt 为一个 [32768,65536] 区间的整数字符串, 不进行定长
+    pub fn get_sign(&self, message: &str) -> String {
+        let strs: [&str; 4]
+            = [&self.id, message, &self.salt.to_string(), &self.key];
+        format!("{:x}", make_md5(&strs.concat()))
+    }
+
+    #[allow(unused)]
+    pub fn from_lang(&self) -> &str {
+        self.from_lang
+    }
+
+    #[allow(unused)]
+    pub fn to_lang(&self) -> &str {
+        self.to_lang
+    }
+}
+
+
+
+struct Config {
+    from_lang: Option<String>,
+    to_lang: Option<String>,
+    text: String,
+    format: Fmtter,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self { 
+            from_lang: None,
+            to_lang: None,
+            text: String::new(),
+            format: Fmtter::build("%s\n%s\n").unwrap()
+        }
+    }
+}
+
+/// out help info and exit
+fn help(code: i32) -> ! {
+    macro_rules! concatn {
+        ( $( $line:expr ),* $(,)? ) => {
+            concat!( $( $line, "\n" ),* )
+        };
+    }
+    eprint!(concatn!{
+        "USAGE: {} [OPTIONS] <FILE>",
+        "OPTIONS:",
+        "    -f               from lang",
+        "    -t               to lang",
+        "    -m               formatter",
+        "    --               stop read options",
+        "    -v, --version    version",
+        "    -h, --help       help",
+        "NOTE:",
+        "    <FILE> is - use stdin",
+        "    config file in {:?},",
+        "        line1: appid, line2: appkey",
+        "Format:",
+        "    |----|-------------|",
+        "    | %s | Display     |",
+        "    | %r | Debug       |",
+        "    | %R | DebugExpand |",
+        "    | %n | LF          |",
+        "    | %N | CR          |",
+        "    | %t | Tab         |",
+        "    | %e | ESC         |",
+        "    |----|-------------|",
+        "    `%[n]...` example: `%0s`, index 0 Display",
+    }, env!("CARGO_BIN_NAME"), config_path());
+    exit(code);
+}
+
+
+fn get_cfg() -> Config {
+    let mut args = args();
+    args.next().unwrap(); // self
+    let mut cfg = Config::default();
+    let mut with_args = false;
+    let mut readed_file = false;
+    macro_rules! get {
+        ( $key:expr ) => {{
+            args.next().unwrap_or_else(|| {
+                eprintln!("key {} no value.", $key);
+                exit(2);
+            })
+        }};
+    }
+    while let Some(i) = args.next() {
+        macro_rules! get_file {
+            ( $path:expr ) => {{
+                let path = $path;
+                if let Some(opt) = args.next() {
+                    eprintln!("redundant options: {:?}", opt);
+                    help(2);
+                }
+                if path == "-" {
+                    // stdin
+                    cfg.text.clear();
+                    stdin().read_to_string(&mut cfg.text)
+                        .unwrap_or_else(|e| {
+                            eprintln!("readstdin error: {}", e);
+                            exit(3);
+                        });
+                } else {
+                    cfg.text = read_to_string(path)
+                        .unwrap_or_else(|e| {
+                            eprintln!("readfile error: {}", e);
+                            exit(3);
+                        });
+                }
+                readed_file = true;
+                break;
+            }};
+        }
+        with_args = true;
+        match &*i {
+            "-h" | "--help" => help(0),
+            "-f" => cfg.from_lang = Some(get!(i)),
+            "-t" => cfg.to_lang = Some(get!(i)),
+            "-m" => cfg.format = Fmtter::build(&get!(i))
+                .unwrap_or_else(|e| {
+                    eprintln!("build fmtter error: {}", e);
+                    exit(2);
+                }),
+            "-v" => {
+                eprintln!("v{}", env!("CARGO_PKG_VERSION"));
+                exit(0);
+            },
+            "--" => {
+                get_file!(get!("FILE"))
+            }
+            path => {
+                // file
+                get_file!(path)
+            }
+        }
+    }
+    if ! with_args {
+        eprintln!("error: no args");
+        help(2);
+    }
+    if ! readed_file {
+        eprintln!("error: no file");
+        help(2);
+    }
+    cfg
+}
+
+
+#[tokio::main]
+async fn main() {
+    let cfg = get_cfg();
+    let [id, key] = get_id_and_key();
+    let mut translater = Translater::new(&id, &key);
+    if let Some(x) = &cfg.from_lang {
+        translater.set_from_lang(x);
+    }
+    if let Some(x) = &cfg.to_lang {
+        translater.set_to_lang(x);
+    }
+    let result: JSONData
+        = translater.translate(cfg.text).await;
+    if let Some(lines) = result.get("trans_result") {
+        for line in lines.as_array().unwrap() {
+            let line = line.as_object().unwrap();
+            let [dst, src]
+                = [
+                line.get("dst").unwrap().as_str().unwrap(),
+                line.get("src").unwrap().as_str().unwrap()
+                ];
+            print!("{}", cfg.format.fmt_str(&[dst, src]));
+        }
+    } else {
+        eprintln!("result data error: {:#?}", result);
+        exit(4);
+    }
+}
